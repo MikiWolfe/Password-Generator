115eec69b81dbd369baceb535e7017ff8a02f457
Mikayla Bruce <mkla.w.b@gmail.com>
2021-11-12 10:58:01 -0800

    Added variables for upper, lower, numbers and special characters.  As well as added alerts with the help of my tutor. Relaized I had not commited those additions.

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
+var specialChar="!#$%&'()*+,-./:;<=>?@[]^`{}|~_";
+var lowerCase="abcdefghijklmnopqrstuvwxyz";
+var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var integer="1234567890";
+
+var userPass=[];
+var finalPass=[];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+ let length = prompt('please choose a password length between 8 and 128 characters');
 
-  passwordText.value = password;
+ if(length <= 8 || length >= 128 || length === '' || length.match(/[a-z]/i) ||
+ length.match(/[A-Z]/i))
+ {
+   alert('please try again')
+ };
 
-console.log('generate a password')
-return password;
+ console.log('length',length)
+
+ var userSpecialChar=confirm("Whould you like special characters in your password?");
+ if (userSpecialChar) {
+   userPass.push(specialChar)
+ };
+ console.log('user pass array',userPass);
+ 
+ var userLowCase=confirm("Would you like lower case letters in your password?");
+ if (userLowCase) {
+   userPass.push(lowerCase)
+ }
+
+ console.log('user pass array',userPass)
+
+ var userUpCase=confirm("Would you like upper case letters in your password?");
+ if (userUpCase) {
+   userPass.push(upperCase)
+ }
+console.log('user pass array',userPass);
+
+ var userInt=confirm("Would you like numbers in your password?");
+ if (userInt) {
+ userPass.push(integer)} 
+ console.log('user pass array',userPass);
+
+ }
 // for loop until password is desired length 
 
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
